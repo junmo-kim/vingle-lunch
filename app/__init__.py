fbc5c1d6d2d8d363d149af0974bdec7a498c56f0
Junmo Kim <lupinogle@gmail.com>
2015-01-18 00:44:22 +0900

    Add materialize and web assets.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,26 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext import assets
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+env = assets.Environment(app)
+
+env.load_path = [
+    os.path.join(os.path.dirname(__file__), '../bower_components'),
+]
+
+
+env.register(
+    'style',
+    assets.Bundle(
+        'materialize/dist/css/materialize.min.css',
+        output='style.css'
+    )
+)
+
 db = SQLAlchemy(app)
 
 from app import views, models
