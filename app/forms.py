d2bdf8688e4452b358193d1efdaecd536ba11bd8
Junmo Kim <lupinogle@gmail.com>
2015-01-19 00:00:54 +0900

    Add gender select field.

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, HiddenField
+from wtforms.fields import SelectField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired
 from app.models import Team
@@ -14,7 +15,7 @@ def enabled_teams():
 class UserForm(Form):
     name = StringField('Name', validators=[DataRequired()])
     team = QuerySelectField('Team', query_factory=enabled_teams, allow_blank=True, blank_text='Choose your team')
-    gender = StringField('gender')
+    gender = SelectField('Gender', choices=[(None, 'Not set'), ('m', 'Male'), ('f', 'Female'), ('q', 'Queer')])
 
 class LunchDataForm(Form):
 	data = HiddenField('Data', validators=[DataRequired()])
